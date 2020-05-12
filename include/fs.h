60b75c6541d5e623339a57c61b60ab4aa107f1b6
Jerry Yu <jyu283@wisc.edu>
2020-05-11 20:19:38 -0500

    Updated data types and added macros.

--- include/fs.h
+++ include/fs.h
@@ -2,21 +2,29 @@
 #define __FS_H__
 
 #include <string.h>
+#include <stdint.h>
 
 #define MAXFSSIZE   0x40000000
 #define BSIZE       0x200      // block size = 512 bytes
 #define INOPERBLK   (BSIZE / sizeof(struct inode))
-
-#define DENTRYNAMELEN   16
 
 /*
  * Simple File System layout diagram:
  * | Superblock | Inodes | Bitmap | Data |
  */
 
+#define T_UNUSED    0
+#define T_REG       1
+#define T_DIR       2
+#define T_DEV       3
+#define NADDR       16
+
 struct inode {
-    unsigned int data[16];
+    uint8_t        type; 
+    uint32_t       data[NADDR];
 };
+
+#define DENTRYNAMELEN   (64 - sizeof(struct inode *))
 
 struct dentry {
     struct inode    *inode;
@@ -24,16 +32,17 @@ struct dentry {
 };
 
 struct superblock {
-    unsigned long   size;       // total size in blocks
-    unsigned long   ninodes;    // number of inodes
-    unsigned long   nblocks;    // number of data blocks
+    uint64_t        size;       // total size in blocks
+    uint64_t        ninodes;    // number of inodes
+    uint64_t        nblocks;    // number of data blocks
     struct inode    *inodes;
     char            *bitmap;
     char            *data;
+    struct dentry   rootdir;
 };
 
 extern char *fs;
 
-void init_superblock(size_t size);
+void init_fs(size_t size);
 
 #endif  // __FS_H__
