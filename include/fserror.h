a2e8d6751732517c73b19b6bde38541438a53bed
Jerry Yu <jyu283@wisc.edu>
2020-05-16 12:39:34 -0500

    added list of standard error codes.

---
+++ include/fserror.h
@@ -0,0 +1,22 @@
+/**
+ * fsemu/include/fserror.h
+ * 
+ * Error codes.
+ */
+
+#ifndef __FSERROR_H__
+#define __FSERROR_H__
+
+#define ENOFOUND	1   // File not found
+#define EEXISTS		2   // File already exists
+#define EALLOC		3   // Allocation failure
+#define EINVTYPE	4   // Invalid inode type
+#define EINVNAME	5   // Invalid name
+#define ENOFD		6   // No available file descriptors
+#define EINVFD		7   // Invalid file descriptor
+#define ENOTEMPTY	8   // Directory not empty (rmdir)
+
+const char *fs_strerror(int errno);
+void fs_pstrerror(int errno, const char *title);
+
+#endif  // __FSERROR_H__
---
+++ src/fserror.c
@@ -0,0 +1,47 @@
+/**
+ * fsemu/src/fserror.c
+ * 
+ * Error codes.
+ * 
+ * For now this is really just a number->string translation.
+ * More will be done here once there is an actual simulated 
+ * "system call" process.
+ */
+
+#include "fserror.h"
+
+#include <stdio.h>
+
+static const char *errors[] = {
+	[0]			"No error",
+	[ENOFOUND]	"No such file or directory",
+	[EEXISTS]	"File or directory already exists",
+	[EALLOC]	"Allocation failed",
+	[EINVTYPE]	"Invalid file type",
+	[EINVNAME]	"Invalid name",
+	[ENOFD]		"No available file descriptor",
+	[EINVFD]	"Invalid file descriptor",
+	[ENOTEMPTY]	"Directory is not empty"
+};
+
+/**
+ * Returns a string explaining the error code.
+ */
+const char *fs_strerror(int errno)
+{
+	static int maxerrno;
+	maxerrno = sizeof(errors) / sizeof(errors[0]);
+	if (errno > 0 || errno < -maxerrno) {
+		return "strerror: bad error code";
+	} else {
+		return errors[-errno];
+	}
+}
+
+/**
+ * Prints out a string for the error code after a title.
+ */
+void fs_pstrerror(int errno, const char *title)
+{
+	printf("%s: %s.\n", title, fs_strerror(errno));
+}
