9425e9c70905ee82cf9d7695b1f7e33562c54561
Jerry Yu <jyu283@wisc.edu>
2020-05-25 23:11:39 -0500

    Added "system call" IDs.

--- include/fs_syscall.h
+++ include/fs_syscall.h
@@ -34,6 +34,48 @@ unsigned int fs_lseek(int fd, unsigned int off);
 unsigned int fs_read(int fd, void *buf, unsigned int count);
 unsigned int fs_write(int fd, void *buf, unsigned int count);
 
+/* system call IDs */
+
+#define SYS_mount	0
+#define SYS_unmount	1
+#define SYS_open	2
+#define SYS_close	3
+#define SYS_unlink	4
+#define SYS_link	5
+#define SYS_mkdir	6
+#define SYS_rmdir	7
+#define SYS_creat	8
+#define SYS_lseek	9
+#define SYS_read	10
+#define SYS_write	11
+
+/* 
+ * System call handler functions, where arguments are retrieved.
+ * 
+ * Handling arguments:
+ * Obivously arguments can't be directly passed through system calls,
+ * so some assembly manipulation is required.
+ */
+
+// mapping from syscall ID to system call handlers, 
+// in which arguments are retrieved.
+/*
+static int (*syscalls[])(void) = {
+	[SYS_mount]		sys_mount,
+	[SYS_unmount]	sys_unmount,
+	[SYS_open]		sys_open,
+	[SYS_close]		sys_close,
+	[SYS_unlink]	sys_unlink,
+	[SYS_link]		sys_link,
+	[SYS_mkdir]		sys_mkdir,
+	[SYS_rmdir]		sys_rmdir,
+	[SYS_creat]		sys_creat,
+	[SYS_lseek]		sys_lseek,
+	[SYS_read]		sys_read,
+	[SYS_write]		sys_write
+};
+*/
+
 // Debug functions
 // If around declarations because these functions should
 // only be called when debugging. 
