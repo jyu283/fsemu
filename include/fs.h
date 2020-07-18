82b32760e624decbc4ffb6a098ae316d98bc9dee
Jerry Yu <jyu283@wisc.edu>
2020-07-18 16:02:31 -0500

    dentry update: use ext4-style dentries.
    + Officially named this file system the Hobbit File System (HFS)
    
    ~ Re-organised dentry to follow an ext4 style, and thereby unifying
      the previously separated inline dentry and regular dentry structs.
    
    ~ Two for_each_xxx_dentry macros for traversing through inline dentries
      and in-block dentries.
    
    ~ All structs now have the hfs_ prefix.
    
    - Removed functions now rendered unnecessary due to the mergeing of
      the two dentry types.
    
    	modified:   include/fs.h

--- include/fs.h
+++ include/fs.h
@@ -1,6 +1,15 @@
 /**
  * fsemu/include/fs.h
  * 
+ * The Hobbit File System (HFS)
+ * 
+ * This user-space pseudo-file system is thus named in the hopes that
+ * it will bear the same qualities as the Hobbits of the Shire -- simple
+ * and peaceful, yet buried deep in its heart is the capacity to change
+ * the world.
+ * 
+ * Eighteenth of July, the Year Two Thousand and Twenty of the Fifth Age
+ * 
  * Defines the parameters and structures of the file system.
  * For now this remains a very basic file system, no more
  * complicated than the one found in the xv6 kernel. 
@@ -23,7 +32,7 @@
 #define BSIZE       0x1000      // block size = 4096 bytes 
 #define DENTCACHESIZE	64
 
-#define INOPERBLK   (BSIZE / sizeof(struct inode))
+#define INOPERBLK   (BSIZE / sizeof(struct hfs_inode))
 #define BLKADDR(x)	((void *)(fs + x * BSIZE))
 
 /* In-memory caches:
@@ -47,22 +56,15 @@
 /* Inode flags */
 #define I_INLINE	0x00000001
 
-/**
- * FIXME:
- * WARNING: Multiple dentry formats.
- * As of now, due to the inline dentry update. There exists
- * two types of dentries. The split dentry design will be 
- * discarded soon in the future, at which point there will be
- * only ONE type of dentry (extendible).
- */
-struct dentry_inline {
+struct hfs_dentry {
 	uint32_t	inum;
-	uint8_t 	reclen;
-	uint8_t 	namelen;
-	char 		name[0];
+	uint16_t	reclen;
+	uint8_t		namelen;
+	uint8_t		file_type;
+	char		name[0];
 };
 
-struct inode {
+struct hfs_inode {
 	uint32_t		nlink;
 	uint32_t		size;
 	uint32_t		flags;
@@ -70,122 +72,70 @@ struct inode {
 		uint32_t	blocks[NBLOCKS];
 		struct {
 			uint32_t				p_inum;
-			struct dentry_inline	dent;
+			struct hfs_dentry		dent_head;
 		} inline_dir;
 	} data;
 	uint8_t			type; 
 };
 
-// For loop to traverse through inline directories
+// For loop to traverse through INLINE directories
+// d:	a hfs_dentry pointer
+// dir:	pointer to an INLINE directory inode
 #define for_each_inline_dent(d, dir) \
-	for (d = &dir->data.inline_dir.dent;\
+	for (d = &dir->data.inline_dir.dent_head;\
 		 (char *)d < (char *)&dir->data + \
-		 		sizeof(dir->data) - sizeof(struct dentry_inline);\
-		 d = (struct dentry_inline *)((char *)d + d->reclen))
+		 		sizeof(dir->data) - sizeof(struct hfs_dentry);\
+		 d = (struct hfs_dentry *)((char *)d + d->reclen))
 
-static inline bool inode_is_inline_dir(struct inode *inode)
+// For loop to traverse through directory file block
+// d:	a hfs_dentry pointer
+// b:	pinter to the start of a directory file block
+#define for_each_block_dent(d, b) \
+	for (d = (struct hfs_dentry *)b;	\
+		 (char *)d + sizeof(struct hfs_dentry) < (char *)b + BSIZE; \
+		 d = (struct hfs_dentry *)((char *)d + d->reclen))
+
+
+static inline bool inode_is_inline_dir(struct hfs_inode *inode)
 {
 	return (inode->flags & I_INLINE);
 }
 
-static inline uint8_t get_inline_dent_reclen(const char *name)
+static inline uint16_t get_dentry_reclen_from_name(const char *name)
 {
-	return (sizeof(struct dentry_inline) + strlen(name) + 1);
+	return (sizeof(struct hfs_dentry) + strlen(name) + 1);
 }
 
-static inline bool inline_dent_can_fit(struct inode *di, 
-										struct dentry_inline *dent,
+static inline bool inline_dent_can_fit(struct hfs_inode *dir, 
+										struct hfs_dentry *dent,
 										uint8_t reclen)
 {
-	return ((char *)dent + reclen < (char *)&di->data + sizeof(di->data));
+	return ((char *)dent + reclen < (char *)&dir->data + sizeof(dir->data));
 }
 
 #define ROOTINO		1
 
 #define DENTRYNAMELEN	255  // Just like in EXT2 and EXT4 
-#define DENTRYSIZE		(sizeof(struct dentry) + sizeof(struct dentry_ext))
-#define DENTPERBLK		(BSIZE / DENTRYSIZE)
-
-/**
- * Split dentry:
- * Each directory block will be split into two sections:
- * The first is a compact lookup table with hash values. (struct dentry)
- * The second (struct dentry_ext) contains the full file names as well 
- * as the inum of the corresponding directory entry.
- */
-
-struct dentry {
-	uint32_t		inum;
-	unsigned int	name_hash;
-};
-
-struct dentry_ext {
-	char			name[DENTRYNAMELEN];
-};
-
-struct dentry_block {
-	struct dentry		dents[DENTPERBLK];
-	struct dentry_ext	ext[DENTPERBLK];
-};
-
-/**
- * Hash function to obtain name_hash in struct dentry.
- */
-static unsigned int dentry_hash(const char *name)
-{
-    unsigned int hash = 5381;
-    int c;
-
-    while ((c = *name++))
-        hash = ((hash << 5) + hash) + c;  // hash * 33 + c
-
-    return hash;
-}
-
-/**
- * Obtain the (detached) extended section of a dentry.
- * 
- * Exploits the fact that blocks are 4K aligned (at least I damn well
- * hope so) to calculate where the extended part corresponding to a
- * dentry should be.
- */
-static inline struct dentry_ext *dentry_get_ext(struct dentry *dent)
-{
-	struct dentry_block *block = (void *)dent - ((unsigned long)dent % BSIZE);
-	int off = ((unsigned long)dent - (unsigned long)block) / sizeof(struct dentry);
-	return &block->ext[off];
-}
 
 /**
  * Getter for dentry name.
  */
-static inline char *dentry_get_name(struct dentry *dent)
+static inline char *dentry_get_name(struct hfs_dentry *dent)
 {
-	return (char *)&dentry_get_ext(dent)->name;
-}
-
-/**
- * Setter for dentry name.
- */
-static inline void dentry_set_name(struct dentry *dent, const char *name)
-{
-	unsigned int name_hash = dentry_hash(name);
-	dent->name_hash = name_hash;
-	strcpy(dentry_get_name(dent), name);
+	return dent->name;
 }
 
 /**
  * Setter for inline dentry name.
  */
-static inline void inline_dentry_set_name(struct dentry_inline *dent, 
-											const char *name)
+static inline void dentry_set_name(struct hfs_dentry *dent, const char *name)
 {
 	uint8_t namelen = strlen(name) + 1;
 	dent->namelen = namelen;
 	strcpy(dent->name, name);
 }
 
-struct superblock {
+struct hfs_superblock {
 	uint64_t		size;       // total size in blocks
 	uint64_t		ninodes;    // number of inodes
 	uint64_t		datastart;	// data start block
@@ -195,29 +145,29 @@ struct superblock {
 };
 
 extern char *fs;
-extern struct superblock *sb;
-extern struct inode *inodes;
+extern struct hfs_superblock *sb;
+extern struct hfs_inode *inodes;
 extern char *bitmap;
 
-static inline struct inode *get_root_inode(void)
+static inline struct hfs_inode *get_root_inode(void)
 {
 	return &inodes[ROOTINO];
 }
 
-static inline struct inode *inode_from_inum(uint32_t inum)
+static inline struct hfs_inode *inode_from_inum(uint32_t inum)
 {
 	return &inodes[inum];
 }
 
-static inline struct inode *dentry_get_inode(struct dentry *dent)
+static inline struct hfs_inode *dentry_get_inode(struct hfs_dentry *dent)
 {
 	return inode_from_inum(dent->inum);
 }
 
-struct dentry *lookup(const char *pathname);
-struct dentry *dir_lookup(const char *pathname, struct inode **pi);
+struct hfs_dentry *lookup(const char *pathname);
+struct hfs_dentry *dir_lookup(const char *pathname, struct hfs_inode **pi);
 
-static inline int inum(struct inode *i)
+static inline int inum(struct hfs_inode *i)
 {
 	return (i - inodes);
 }
